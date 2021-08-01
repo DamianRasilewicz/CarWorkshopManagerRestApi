baeb88ccf0bd9a278f2a522689795434e3253243
DamianRasilewicz <rasil754@gmail.com>
2021-08-01 19:24:56 +0200

    Inicial commit

---
+++ src/main/java/pl/rasilewicz/carworkshopmanagerrestapi/CarworkshopmanagerrestapiApplication.java
@@ -0,0 +1,13 @@
+package pl.rasilewicz.carworkshopmanagerrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarworkshopmanagerrestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarworkshopmanagerrestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/rasilewicz/carworkshopmanagerrestapi/CarworkshopmanagerrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rasilewicz.carworkshopmanagerrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarworkshopmanagerrestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
