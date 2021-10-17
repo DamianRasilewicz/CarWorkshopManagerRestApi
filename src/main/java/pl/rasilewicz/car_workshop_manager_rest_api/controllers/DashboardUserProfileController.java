b597ce568ff7fef1aac5500ed330577684d604b6
DamianRasielwicz <rasil754@gmail.com>
2021-10-17 20:23:54 +0200

    Change userProfile and changedUserProfile endpoints to REST API endpoints

--- src/main/java/pl/rasilewicz/car_workshop_manager_rest_api/controllers/DashboardUserProfileController.java
+++ src/main/java/pl/rasilewicz/car_workshop_manager_rest_api/controllers/DashboardUserProfileController.java
@@ -3,11 +3,7 @@ package pl.rasilewicz.car_workshop_manager_rest_api.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.mindrot.jbcrypt.BCrypt;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.rasilewicz.car_workshop_manager_rest_api.entities.Role;
 import pl.rasilewicz.car_workshop_manager_rest_api.entities.User;
 import pl.rasilewicz.car_workshop_manager_rest_api.services.RoleServiceImpl;
@@ -22,33 +18,30 @@ public class DashboardUserProfileController {
     private final UserServiceImpl userService;
     private final RoleServiceImpl roleService;
 
+    @GetMapping("/users/{userId}/profile")
+    public User userProfile (@PathVariable Integer userId){
 
-    @GetMapping("/dashboard/user/profile")
-    public String userProfile (Model model, HttpSession session){
-        User userProfile = userService.findUserById((Integer)session.getAttribute("userId"));
-        model.addAttribute("userProfile", userProfile);
-
-        String newPassword = "";
-        model.addAttribute("newPassword", newPassword);
-
-        return "dashboardPages/user/userProfile";
+        return userService.findUserById(userId);
     }
 
-    @PostMapping("/dashboard/user/profile")
-    public String changedUserProfile (User userProfile, String newPassword, HttpSession session){
+    @PutMapping("/users/{userId}/profile")
+    public User changedUserProfile (@RequestBody User user){
 
-        if (!newPassword.equals("")){
-            userProfile.setPassword(BCrypt.hashpw(newPassword, BCrypt.gensalt(12)));
-        } else {
-            userProfile.setPassword(userProfile.getPassword());
+        User userProfile = userService.findUserById(user.getId());
+
+        if (!user.getPassword().equals(userProfile.getPassword())){
+            userProfile.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(12)));
         }
+
         Role userRole = roleService.findRoleByName("USER");
         userProfile.setRole(userRole);
+        userProfile.setPhoneNumber(user.getPhoneNumber());
+        userProfile.setEmail(user.getEmail());
+        userProfile.setLastName(user.getLastName());
+        userProfile.setFirstName(user.getFirstName());
+        userProfile.setUserName(user.getUserName());
         userService.save(userProfile);
 
-        session.removeAttribute("userName");
-        session.setAttribute("userName", userProfile.getUserName());
-
-        return "redirect:/dashboard/user/profile?success";
+        return userProfile;
     }
 }
