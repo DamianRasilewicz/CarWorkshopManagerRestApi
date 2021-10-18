c0756aaf3f6ca1cf5ea8c2aed023977539e27492
DamianRasielwicz <rasil754@gmail.com>
2021-10-18 21:28:48 +0200

    Fixed swagger permisiton denied form spring security

--- src/main/java/pl/rasilewicz/car_workshop_manager_rest_api/configuration/SecurityConfig.java
+++ src/main/java/pl/rasilewicz/car_workshop_manager_rest_api/configuration/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -25,15 +26,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        //swagger
+        web.ignoring().antMatchers(
+                "/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**",
+                "/swagger/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http
                 .authorizeRequests()
                 .antMatchers("/swagger-ui.html").permitAll()
                 .antMatchers("v2/api-docs").permitAll()
+                .antMatchers("**/swagger-resources/**").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
                 .antMatchers("webjars/**").permitAll()
-                .antMatchers("swagger-resources/**").permitAll()
                 .antMatchers("h2-console/**").permitAll()
+                .antMatchers("/v3/api-docs/**").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll();
